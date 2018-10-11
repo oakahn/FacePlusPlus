e576b23004617d46716bb4d12e1c7f4cab9864b6
ChayawatS <mjpoak@gmail.com>
2018-10-11 15:45:36 +0700

    Initial Commit

---
+++ FacePlusPlus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FacePlusPlus
+//
+//  Created by chayarak on 11/10/2561 BE.
+//  Copyright © 2561 chayarak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
